72ad10951f266d738d246920491cda8df6b6a7e5
Aisha <aishaprograms@gmail.com>
2016-11-01 23:50:13 -0500

    Somewhat functional game with odd timing, no reset, no mesage if answer was not selected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
-var startInstructions = 'Click on the button to start the game. Remember you have only 15 seconds to answer each question!';
-var gameStarted = false;
-var gameEnded = false;
+var gameIndex = 0;
+var numCorrect = 0;
+var numWrong = 0;
+var totalTime;
+var questionInterval;
 //object prototype for Question
 function Question(question, answers, correctAnswer, image) {
     this.question = question;
@@ -17,46 +19,89 @@ var q5 = new Question('Which grade of olive oil is considered the best?', ['extr
 //array of all questions
 var gameQuestions = [q1, q2, q3, q4, q5];
 
+function clearView() {
+    $('#time-remaining').text('');
+    $('#question').text('');
+    $('#answers').html('');
+    $('#game-update').text('');
+    $('#correct-answer').text('');
+    $('#image').html('');
+    $('#results').html('');
+    $('#button-section').html('');
+}
 
 function makeStartButton() {
     var startButton = '<button class="btn btn-success" id="start-button">Start</button>';
+    var startInstructions = 'Click on the button to start the game. Remember you have only 15 seconds to answer each question!';
     $('#button-section').html(startButton);
     $('#game-update').text(startInstructions);
-    $('#start-button').on('click', startGame);
 }
 
-function startGame() {
-    gameStarted = true;
-    showQA();
-    $('#button-section').html('');
-    $('#game-update').text('');
-    return;
+function showQA(i) {
+    clearView();
+    var q = gameQuestions[i];
+    $('#question').text(q.question);
+    $('#answers').html('<ul id="answers-list"></ul>');
+    for (var j = 0; j < q.answers.length; j++) {
+        var a = '<li>' + q.answers[j] + '</li>';
+        $('#answers-list').append(a);
+    }
 }
 
-var i = 0;
+function clickAns(i) {
+    $('#answers-list').on('click', 'li', function() {
+        clearInterval(questionInterval);
+        var clicked = $(this).text();
+        clearView();
+        if (clicked === gameQuestions[i].correctAnswer) {
+            $('#game-update').text('You are correct!');
 
-function showQA() {
-    setTimeout(function() {
-        var q = gameQuestions[i];
-        $('#question').text(q.question);
-        $('#answers').html('<ul id="answers-list"></ul>');
-        for (var j = 0; j < q.answers.length; j++) {
-            var a = '<li>' + q.answers[j] + '</li>';
-            $('#answers-list').append(a);
+        } else {
+            $('#game-update').text("Wrong answer!");
         }
-        i++;
-        if (i < gameQuestions.length) {
-            showQA();
-        }
-    }, 2 * 1000);
+        $('#time-remaining').text('Time remaining: ' + totalTime);
+        $('#correct-answer').text('The correct answer is: ' + gameQuestions[i].correctAnswer);
+    });
 }
 
-function clickAns() {
-    $('#answers-list').on('click', 'li', function() {});
+function showResults() {
+    clearView();
+    $('#results').append('Number of correctly answered: ' + numCorrect + '</br>');
+    $('#results').append('Number of incorrectly answered: ' + numWrong + '</br>');
+    if (numCorrect >= numWrong) {
+        $('#results').append('You win!');
+    } else {
+        $('#results').append('You lose...');
+
+    }
+}
+
+function questionTimer() {
+    totalTime = 15;
+    questionInterval = setInterval(function() {
+        $('#time-remaining').text('Time remaining: ' + totalTime);
+        totalTime--;
+        if (totalTime === -1) {
+            clearInterval(questionInterval);
+        }
+    }, 1000);
+
 }
 
 $(document).ready(function() {
-    if (!gameStarted && !gameEnded) {
-        makeStartButton();
+    makeStartButton();
+    $('#start-button').on('click', function() {
+        var gameInterval = setInterval(function() {
+            showQA(gameIndex);
+            questionTimer();
+            clickAns(gameIndex);
+            gameIndex++;
+            if (gameIndex === gameQuestions.length) {
+                clearInterval(gameInterval);
+            }
+        }, 5000);
+    });
+    if (gameIndex === gameQuestions.length) {
+        showResults();
     }
 });
